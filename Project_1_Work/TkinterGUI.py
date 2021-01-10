38c7bdbce1f722ead7e5e74a0778b11e4d1339f6
JakeRivett31 <jake.rivett@ucc.on.ca>
2021-01-10 14:32:58 -0500

    I'm Done

---
+++ Project_1_Work/TkinterGUI.py
@@ -0,0 +1,11 @@
+root = tk.Tk()
+
+
+
+root.config(bg="black")
+
+title = tk.Label(root, text = "Star Wars Quiz and Database")
+#Configuration
+title.config(font =("News Gothic", "50"), fg = "yellow", bg = "black")
+
+title.grid(row=0, column=0, columnspan=2)
